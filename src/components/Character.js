d6e94dbef92911238bfa82de1e0b289b7112d349
Mark <markgabon@hotmail.com>
2021-12-10 12:43:52 -0500

    First commit, finished tasks.

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+const Characters = props => {
+
+const characters = props.characters
+
+    return (
+        <div className="characters">
+            {
+characters.map( person => (
+    <h1 key={person.name}>{person.name}</h1>
+)) 
+}
+        </div>
+)
+}
+
+export default Characters;
